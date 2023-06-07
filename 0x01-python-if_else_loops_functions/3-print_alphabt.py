49cf9dba0bc2e61a2528407c256581ad82a5f47e
Angiecodes <angelcoder87@gmail.com>
2023-06-07 16:40:34 +0300

    Print alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3	
-for i in range(ord('a'), ord('z') + 1):
-	if chr(i) not in ['q', 'e']:
-		print(chr(i), end='')
-
+#!/usr/bin/python3
+for i in range(97, 123):
+    if (i == 101) or (i == 113):
+        continue
+    print(chr(i).format(), end="")
