debb7db518b72a3c454834840a4dbd48e5a65dea
Angiecodes <angelcoder87@gmail.com>
2023-06-08 17:34:52 +0300

    Who are you

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-import hidden_4
-
-def print_module_names():
-    """Print all names defined by the hidden_4 module."""
-    names = [name for name in dir(hidden_4) if not name.startswith('__')]
-    for name in names:
-    print(name)
 
 if __name__ == "__main__":
-    print_module_names()
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
