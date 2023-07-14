3c5ceb12550dd8ee6889821b806b9439795c2c9c
Angiecodes <angelcoder87@gmail.com>
2023-07-14 10:06:23 +0300

    Square 2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Defines a Rectangle subclass Square."""
+
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """Calculate the area of the rectangle."""
+        return self.width * self.height
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def integer_validator(self, name, value):
+        """Validate the integer value."""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
