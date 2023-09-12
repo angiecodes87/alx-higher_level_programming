5f2260c3063ad2f2e4f681366e91a906e5c172e6
Angiecodes <angelcoder87@gmail.com>
2023-09-12 22:45:25 +0300

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,6 +10,11 @@ def read_file(filename=""):
         filename (str): The name of the file to be read. (default: "")
 
     """
-    with open(filename, 'r', encoding='utf-8') as file:
-        content = file.read()
-        print(content)
+    with open(filename, encoding='utf-8') as file:
+        for line in file:
+            print(line, end="")
+
+
+# Example usage:
+if __name__ == "__main__":
+    read_file("my_file_0.txt")
