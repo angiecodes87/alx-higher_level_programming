912b155db910d4df7e9b31cf014259860a5cd743
Angiecodes <angelcoder87@gmail.com>
2023-09-12 22:53:30 +0300

    Add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """Add all arguments to a Python list and save them to a file."""
-
-
 import sys
+import json
 from os.path import exists
-from load_from_json_file import load_from_json_file
 from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
-filename = "add_item.json"
 
-if exists(filename):
-    items = load_from_json_file(filename)
-else:
-    items = []
+def add_item(args_list):
+    filename = "add_item.json"
 
-items.extend(sys.argv[1:])
+    if exists(filename):
+        my_list = load_from_json_file(filename
+    else:
+        my_list = []
 
-save_to_json_file(items, filename)
+    my_list.extend(args_list)
+    save_to_json_file(my_list, filename)
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    add_item(args)
