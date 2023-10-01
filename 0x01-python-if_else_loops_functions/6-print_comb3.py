1ebe13bd77a986c427b8bb736bbea7f1b9b9ee3b
Angiecodes <angelcoder87@gmail.com>
2023-10-01 17:02:34 +0300

    New changes

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-for i in range(9):
-    for j in range(i + 1, 10):
-        if i * 10 + j < 89:
-            print("{:d}{:d}".format(i, j), end=", ")
-    print("{:d}".format(89))
+
+for tens_digit in range(10):
+    for ones_digit in range(tens_digit + 1, 10):
+        if tens_digit < 9:
+            print("{:d}{:d}, ".format(tens_digit, ones_digit), end="")
+        else:
+            print("{:d}{:d}".format(tens_digit, ones_digit))
+
+print()  # Print a newline at the end
