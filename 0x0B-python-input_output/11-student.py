e07a9af5069c9331571eeb260fcb8539aeffc4e3
Angiecodes <angelcoder87@gmail.com>
2023-10-01 22:23:49 +0300

    New changes

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -20,9 +20,9 @@ class Student:
         """
             Initialises Student instance.
         """
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self, attr=None):
         """
