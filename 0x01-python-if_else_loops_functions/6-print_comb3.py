a1d9f10940cdbbc932f7dd309848c9d18d2f3d95
Angiecodes <angelcoder87@gmail.com>
2023-10-01 17:10:51 +0300

    New changes

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 
 for tens_digit in range(10):
     for ones_digit in range(tens_digit + 1, 10):
-        if tens_digit < 9:
+        if ones_digit < 9:  # Change the condition here
             print("{:d}{:d}, ".format(tens_digit, ones_digit), end="")
         else:
             print("{:d}{:d}".format(tens_digit, ones_digit))
