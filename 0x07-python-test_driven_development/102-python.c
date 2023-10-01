8f66bfb2973eb3d24c6db5577f4aad25ee52954e
Angela Odhiambo <128974810+angiecodes87@users.noreply.github.com>
2023-10-02 00:23:15 +0300

    Update 102-python.c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,28 +1,39 @@
 #include "Python.h"
 
 /**
- * print_python_string - Prints information about Python strings.
- * @p: A PyObject string object.
+ * print_python_string_info - Prints information about a Python string object.
+ * @p: A PyObject representing a string.
  */
-void print_python_string(PyObject *p)
+void print_python_string_info(PyObject *p)
 {
-	long int length;
+    long int length;
 
-	fflush(stdout);
+    fflush(stdout);
 
-	printf("[.] string object info\n");
-	if (strcmp(p->ob_type->tp_name, "str") != 0)
-	{
-		printf("  [ERROR] Invalid String Object\n");
-		return;
-	}
+    // Print information about the Python string object
+    printf("[.] String Object Info\n");
 
-	length = ((PyASCIIObject *)(p))->length;
+    // Check if the object is a valid string
+    if (strcmp(p->ob_type->tp_name, "str") != 0)
+    {
+        printf("  [ERROR] Invalid String Object\n");
+        return;
+    }
 
-	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf("  type: compact ascii\n");
-	else
-		printf("  type: compact unicode object\n");
-	printf("  length: %ld\n", length);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+    // Get the length of the string
+    length = PyUnicode_GET_LENGTH(p);
+
+    // Determine the type of the string (compact ASCII or Unicode)
+    if (PyUnicode_IS_COMPACT_ASCII(p))
+    {
+        printf("  Type: Compact ASCII\n");
+    }
+    else
+    {
+        printf("  Type: Compact Unicode Object\n");
+    }
+
+    // Print the length and value of the string
+    printf("  Length: %ld\n", length);
+    printf("  Value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
