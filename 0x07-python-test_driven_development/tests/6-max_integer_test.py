fb8858c148e7d5878f90c18fbe281f8f8907cc62
Angela Odhiambo <128974810+angiecodes87@users.noreply.github.com>
2023-10-02 01:14:40 +0300

    Update 6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,17 +1,18 @@
 """
-    unit test for 6-max_integer.py
+Unit test for max_integer function.
 """
-import unittest
-max_integer = __import__('6-max_integer').max_integer
 
+import unittest
+from your_module import max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    def test_max(self):
+    def test_max_integer(self):
         self.assertAlmostEqual(max_integer([4, 5, 7]), 7)
         self.assertAlmostEqual(max_integer([8, 234, 332, 33]), 332)
         self.assertAlmostEqual(max_integer([-1, -2, -3]), -1)
         self.assertAlmostEqual(max_integer([20000000022]), 20000000022)
         self.assertAlmostEqual(max_integer([]), None)
 
-    def test_non_int(self):
+    def test_non_integer_input(self):
         self.assertRaises(TypeError, max_integer, True)
+
