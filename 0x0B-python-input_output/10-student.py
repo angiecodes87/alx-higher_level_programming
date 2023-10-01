8a1498415992bcc3b6302f75f717b6526bd5b0da
Angela Odhiambo <128974810+angiecodes87@users.noreply.github.com>
2023-10-02 00:52:01 +0300

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,37 +1,29 @@
 #!/usr/bin/python3
-"""
-    10-student: class Student
-"""
+"""Defines a class Student."""
 
 
 class Student:
-    """
-        A class students that defines a student by:
-        Attributes:
-            first_name (str): name of student.
-            last_name (str): name of student.
-            age (int): age of student.
-        Methods:
-            __init__ - initializes the Student instance.
-            to_json - retrieves dictionary repr of Student instance.
-    """
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
-        """
-            Initialises Student instance.
-        """
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
-
-def to_json(self, attr=None):
-    """
-        retrieves a dictionary representation of Student.
+        """Initialize a new Student.
         Args:
-            attr (list): attribute names that are to be retrieved.
-    """
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-    if attr is not None:
-        res = {k: self.__dict__[k] for k in self.__dict__.keys() & attr}
-        return res
-    else:
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
