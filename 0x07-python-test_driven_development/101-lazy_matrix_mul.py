4782930c5703294ffaa5379ab4981254a47b9306
Angela Odhiambo <128974810+angiecodes87@users.noreply.github.com>
2023-10-02 00:34:09 +0300

    Update 101-lazy_matrix_mul.py

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,28 +1,10 @@
-#include "Python.h"
+#!/usr/bin/python3
+"""
+Defines lazy_matrix function
+"""
+import numpy
 
-/**
- * print_python_string - Displays information about Python strings.
- * @p: A PyObject representing a string.
- */
-void print_python_string(PyObject *p)
-{
-	long int length;
 
-	fflush(stdout);
-
-	printf("[.] string object info\n");
-	if (strcmp(p->ob_type->tp_name, "str") != 0)
-	{
-		printf("  [ERROR] Invalid String Object\n");
-		return;
-	}
-
-	length = ((PyASCIIObject *)(p))->length;
-
-	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf("  type: compact ascii\n");
-	else
-		printf("  type: compact unicode object\n");
-	printf("  length: %ld\n", length);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
-}
+def lazy_matrix_mul(m_a, m_b):
+    """ calculates the matrix multiplication of two matrices"""
+    return numpy.matmul(m_a, m_b)
