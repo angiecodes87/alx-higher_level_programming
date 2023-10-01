1c37da1ba6f45e281eb06a0d22f6ed77bfd206d5
Angela Odhiambo <128974810+angiecodes87@users.noreply.github.com>
2023-10-02 00:26:08 +0300

    Update 101-lazy_matrix_mul.py

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,36 +1,23 @@
 #!/usr/bin/python3
 """
-This script defines a function for lazy matrix multiplication using NumPy.
+Defines a function for matrix multiplication using NumPy.
 """
-
 
 import numpy as np
 
-def lazy_matrix_mul(m_a, m_b):
+def matrix_multiplication(m_a, m_b):
     """
-    Multiply two matrices represented as NumPy arrays.
+    Multiply two matrices using NumPy arrays.
 
     Args:
-        m_a (list of lists): The first matrix.
-        m_b (list of lists): The second matrix.
+        m_a (numpy.ndarray): The first matrix.
+        m_b (numpy.ndarray): The second matrix.
 
     Returns:
         numpy.ndarray: The result of matrix multiplication.
-
-    Raises:
-        ValueError: If the input matrices are incompatible for multiplication.
     """
-    if not isinstance(m_a, list) or not isinstance(m_b, list):
-        raise TypeError("m_a and m_b must be lists")
-    
-    if not m_a or not m_b:
-        raise ValueError("m_a can't be empty" if not m_a else "m_b can't be empty")
-    
-    matrix_a = np.array(m_a)
-    matrix_b = np.array(m_b)
+    if not isinstance(m_a, np.ndarray) or not isinstance(m_b, np.ndarray):
+        raise TypeError("m_a and m_b must be NumPy arrays")
 
-    if matrix_a.shape[1] != matrix_b.shape[0]:
-        raise ValueError("Matrix dimensions are not compatible for multiplication")
-    
-    result_matrix = np.matmul(matrix_a, matrix_b)
+    result_matrix = np.matmul(m_a, m_b)
     return result_matrix
